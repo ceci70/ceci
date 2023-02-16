e87b95ebc2392b235294135e965442ff9eaa89b7
ceci70 <125604984+ceci70@users.noreply.github.com>
2023-02-16 17:29:59 -0300

    Create Sintaxis.js

---
+++ Sintaxis.js
@@ -0,0 +1,40 @@
+document.getElementById('boton').onclick = function(){
+document.getElementById("demo").innerHTML = "daisy.palmer@example.com";
+}
+
+document.getElementById('boton3').onclick = function(){
+document.getElementById("demo3").innerHTML = "8484 Samaritan Dr";
+}
+
+
+document.getElementById('boton2').onclick = function(){
+document.getElementById("demo2").innerHTML = "(218) 975-6766";
+}
+
+document.getElementById('boton4').onclick = function(){
+document.getElementById("demo4").innerHTML = "30 años";
+}
+
+document.getElementById('boton5').onclick = function(){
+document.getElementById("demo5").innerHTML = "Tareas administrativas: Atención al cliente, facturación, cobranzas y trámites bancarios";
+}
+
+document.getElementById('boton6').onclick = function(){
+document.getElementById("demo6").innerHTML = "Analista de impuestos: Ingresos brutos-ARBA etc";
+ }
+
+document.getElementById('boton7').onclick = function(){
+document.getElementById("demo7").innerHTML = "Jefa de Impuestos";
+}
+
+document.getElementById('boton8').onclick = function(){
+document.getElementById("demo8").innerHTML = "Bachiller Normal 8 Presidente Julio A.Roca";
+}
+
+document.getElementById('boton9').onclick = function(){
+document.getElementById("demo9").innerHTML = "Facultad de Ciencias Económicas - Universidad de Buenos Aires";
+}
+
+document.getElementById('boton10').onclick = function(){
+document.getElementById("demo10").innerHTML = "Viajar, ir al cine, observación de aves";
+}
